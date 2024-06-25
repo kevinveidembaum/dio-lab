1776605c425c94c1cc23bf3f7d121103ea46ab9e
Kevin Nikolai <nikolai10052005@gmail.com>
2024-06-25 19:19:22 -0300

    feature: function for exibir_extrato

--- bank.py
+++ bank.py
@@ -32,7 +32,13 @@ def deposito(saldo: float):
 def saque(saldo: float):
     saque = obter_valor("Qual o valor da quantia que deseja sacar: ")
     saldo -= saque
-    return saque, saldo
+    return saldo, saque
+
+def exibir_extrato(saldo: float, extrato: dict):
+    decoracao('Extrato')
+    print(f"Seu Saldo atual corresponde a: R${saldo:.2f}")
+    print(f"Saques: {extrato['saque']}")
+    print(f"Depósitos: {extrato['deposito']}")
 
 def banco():
     LIMITE_SAQUES = 3
@@ -49,15 +55,12 @@ def banco():
 
         if escolha == 1: #Depósito
             decoracao('Depósito')
-            action_depositar = deposito(saldo)
-            valor_deposito = action_depositar[1] #Pega o valor deposito da funcao deposito()
-            saldo = action_depositar[0] #Pega o valor do saldo da funcao deposito()
+            saldo, valor_deposito = deposito(saldo)
             extrato['deposito'].append(valor_deposito) #armazena o valor do deposito no extrato
         elif escolha == 2: #Saque
             decoracao('Saque')
-            action_sacar = saque(saldo)
-            valor_saque = action_sacar[0] #Pega o valor do saque da funcao saque()
-
+            saldo, valor_saque = saque(saldo)
+            
             insuficiente_saldo = valor_saque > saldo 
 
             limite_saque_dia = len(extrato["saque"]) == LIMITE_SAQUES
@@ -73,14 +76,10 @@ def banco():
                 print("Você ultrapassou o valor limite de saque de R$500,00")
                 print("Transação cancelada!")
             else:
-                saldo = action_sacar[1] #Pega o valor do saldo da funcao saque()
                 extrato['saque'].append(valor_saque) #armazena o valor do saque no extrato
                 print(f"Transação realizada com sucesso.")
         elif escolha == 3: #Extrato
-            decoracao('Extrato')
-            print(f"Seu Saldo atual corresponde a: R${saldo:.2f}")
-            print(f"Saques: {extrato['saque']}")
-            print(f"Depósitos: {extrato['deposito']}")
+            exibir_extrato(saldo, extrato)
         elif escolha == 0:
             print('~'*30)
             print("Programa encerrado com sucesso!")
