355ea98201a278d9ec0b551a4a08800d8985f70d
Kevin Nikolai <nikolai10052005@gmail.com>
2024-07-02 10:27:04 -0300

    add: class Conta Structure

--- bank/bank_v2.py
+++ bank/bank_v2.py
@@ -1,6 +1,5 @@
 from datetime import date
 
-
 class PessoaFisica:
     def __init__(self, cpf: str, nome: str, data_nascimento: date) -> None:
         self._cpf = cpf
@@ -21,9 +20,50 @@ class Cliente(PessoaFisica):
     
     def __str__(self) -> str:
         return super().__str__() + f', Endereço: {self._endereco}, Contas: {self._contas}'
+    
+
+    #def realizar_transacao(conta: Conta, transacao: Transacao):
+        
+    
+
+    #def adicionar_conta(conta: Conta):
+
+    
+
+class Conta:
+    def __init__(self, saldo: float, numero: int, agencia: str, cliente: Cliente, historico) -> None:
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self.historico = historico
+
+
+    def __str__(self) -> str:
+        return f'Saldo: {self._saldo}, Agência: {self._agencia}, Cliente: {self._cliente._nome}'
+
+
+    def saldo(self) -> float:
+        return self._saldo
+
+
+    @classmethod
+    def nova_conta(cls, cliente: Cliente, numero: int):
+        pass
+
+
+    def sacar(valor: float) -> bool:
+        pass
+
+
+    def depositar(valor: float) -> bool:
+        pass
+
 
 
 birth_date = date(1990, 12, 4)
 
 c = Cliente('1234', 'Kev', birth_date, 'qwer', [1, 2])
-print(c.__str__())
+conta = Conta(1200, 123, 'itau', c, 0)
+print(conta.__str__())
+print(conta.saldo())
