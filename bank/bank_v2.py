a8a1e19d329c2ed59cf0c3b9ce75d5bdddec51a7
Kevin Nikolai <nikolai10052005@gmail.com>
2024-07-02 09:58:57 -0300

    add: class Cliente

--- bank/bank_v2.py
+++ bank/bank_v2.py
@@ -2,15 +2,28 @@ from datetime import date
 
 
 class PessoaFisica:
-    def __init__(self, cpf: str, nome: str, data_nascimento: date ) -> None:
+    def __init__(self, cpf: str, nome: str, data_nascimento: date) -> None:
         self._cpf = cpf
         self._nome = nome
         self._data_nascimento = data_nascimento
 
+
     def __str__(self) -> str:
-        return f'Nome: {self._cpf}, CPF: {self._cpf}, Data de Nascimento: {self._data_nascimento}'
+        return f'Nome: {self._nome}, CPF: {self._cpf}, Data de Nascimento: {self._data_nascimento}'
 
 
+class Cliente(PessoaFisica):
+    def __init__(self, cpf: str, nome: str, data_nascimento: date, endereco: str, contas: list) -> None:
+        super().__init__(cpf, nome, data_nascimento)
+        self._endereco = endereco
+        self._contas = contas
 
-p = PessoaFisica('1234', 'Kev', 123)
-print(p.__str__())
+    
+    def __str__(self) -> str:
+        return super().__str__() + f', Endereço: {self._endereco}, Contas: {self._contas}'
+
+
+birth_date = date(1990, 12, 4)
+
+c = Cliente('1234', 'Kev', birth_date, 'qwer', [1, 2])
+print(c.__str__())
